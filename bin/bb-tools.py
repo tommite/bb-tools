356d4aac99c8a40bd66e8d535fb2faff1b3b5f21
Tommi Tervonen <tervonen@ese.eur.nl>
2013-03-04 16:01:19 +0100

    Update copyright to 2013

--- bin/bb-tools.py
+++ bin/bb-tools.py
@@ -3,7 +3,7 @@
 ###
 #   This file is part of bb-tools.
 #
-#   (c) Tommi Tervonen and Damir Vandic, 2011-12.
+#   (c) Tommi Tervonen and Damir Vandic, 2011-13.
 #
 #   bb-tools is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
